863891c72a178ec965c59ce10366f24cbd33d188
mahmoudgaber97 <94110835+mahmoudgaber97@users.noreply.github.com>
2022-05-27 00:54:12 +0200

    Update Squar_root_func.c

--- Squar_root_func/Squar_root_func.c
+++ Squar_root_func/Squar_root_func.c
@@ -12,10 +12,10 @@ int main (){
 	do{
 	printf("enter number:");
 	scanf("%d",&u32Input);
-	u32SquareRoot=Fun_u32GetSquarRoot(u32Input);
-	printf("%d\n",u32SquareRoot);
+	u32SquareRoot=Fun_u32GetSquarRoot(u32Input);/*Get value of square root function*/
+	printf("%d\n",u32SquareRoot);  
 	}
-	while(getch()!='E');
+	while(getch()!='E'); /*exit from main when user write 'E'*/
 	return 0;
 }
 
@@ -24,8 +24,8 @@ u32 Fun_u32GetSquarRoot(u32 u32Num){
 	u32 u32Counter=0;
 	u32 u32Reslut;
 
-	while(u32Reslut!=u32Counter){
-		u32Counter++;
+	while(u32Reslut!=u32Counter){      /*when counter is equal result of number/counter it means counter is equal square root*/
+		u32Counter++;              /*increament counter to chack square root of number*/
 		u32Reslut=u32Num/u32Counter;	
 	}
 	printf("The Squar root is ====> ");
