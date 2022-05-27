c9629362b8c95f61260a438717d0f8c0328e6a6d
mahmoudgaber97 <94110835+mahmoudgaber97@users.noreply.github.com>
2022-05-27 22:03:52 +0200

    Add files via upload
    c function to check whether the num base 2 or not?

---
+++ BASED_two/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"TYPE.h"
+enum Boolean{false,true};
+u8 Chack_u8Based2(u32 u32Iputnumber);
+u8 Chack_u8Based2(u32 u32Iputnumber){
+	if(u32Iputnumber!=0 &&(u32Iputnumber&(u32Iputnumber-1))==0)/*
+	
+	if(u32Iputnumber!=0) &&             (u32Iputnumber)&(u32Iputnumber-1)==0
+	
+	*/
+	return true;
+	else
+	return false;
+	
+}
+int main(){
+	
+	u32 u32MainInput;
+	printf("enter ur input number : ");
+	scanf("%d",&u32MainInput);
+	Chack_u8Based2(u32MainInput)?printf("Based on two"):printf("it does not based on two");
+	
+	return 0;
+}
