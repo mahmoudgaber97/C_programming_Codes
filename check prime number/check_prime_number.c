aa60d103c047ebdbd072b269053f6e6265486d50
mahmoudgaber97 <94110835+mahmoudgaber97@users.noreply.github.com>
2022-05-27 03:03:16 +0200

    Add files via upload

---
+++ check prime number/check_prime_number.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include"TYPE.h"
+u32 Chack_u32Number(u32 u32Number);
+u32 Chack_u32Number(u32 u32Number){
+	u32 u32Counter=2,u32Flag=0;
+	if(u32Number==2)
+		u32Flag=0;
+	else{
+	while(u32Counter <= u32Number/2){
+		if(u32Number%u32Counter==0){
+			u32Flag=1;
+		break;
+		}
+		else
+			u32Flag=0;
+		u32Counter++;
+	}
+	}
+	
+	return u32Flag;
+}
+int main(){
+	u32 u32Input;
+	printf("Enter Number you have chack it : ");
+	scanf("%d",&u32Input);/*
+	if(Chack_u32Number(u32Input)==0)
+		printf("The number is prime");
+	else
+	printf("The number is NOT prime");
+*/
+
+/*------------------------OR------------------------*/
+(Chack_u32Number(u32Input)==0)?printf("The number is prime"):printf("The number is NOT prime");
+
+	return 0;
+}
